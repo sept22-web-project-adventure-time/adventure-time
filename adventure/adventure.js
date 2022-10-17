06147556ff6006acade99162a42e2788fba572e4
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-10-17 16:02:25 -0700

    Starting of renderAdventure

--- adventure/adventure.js
+++ adventure/adventure.js
@@ -2,6 +2,7 @@
 // import './auth/user.js';
 
 import { getCoffeeShops, getActivities, getEateries } from '../fetch-utils.js';
+import { renderAdventure } from '../render-utils.js';
 import { getRandomItem } from '../utils.js';
 
 // DOM
@@ -14,6 +15,14 @@ let coffeeShops = [];
 let eateries = [];
 let activities = [];
 
+let adventures = [
+    {
+        coffeeShop: '',
+        eatery: '',
+        activity: '',
+    },
+];
+
 // Events
 window.addEventListener('load', async () => {
     const response = await getCoffeeShops();
@@ -21,16 +30,20 @@ window.addEventListener('load', async () => {
     coffeeShops = response.data;
 
     const coffeeShop = getRandomItem(coffeeShops);
-
+    adventures.coffeeShop = coffeeShop;
     const response1 = await getActivities();
 
     activities = response1.data;
     const activity = getRandomItem(activities);
+    adventures.activity = activity;
 
     const response2 = await getEateries();
     eateries = response2.data;
 
     const eatery = getRandomItem(eateries);
+    adventures.eatery = eatery;
+
+    renderAdventure(adventures);
 
     // console.log(coffeeShop);
     // console.log(activity);
--- render-utils.js
+++ render-utils.js
@@ -1,1 +1,8 @@
-export function renderAdventure(adventure) {}
+export function renderAdventure(adventure) {
+    const li = document.createElement('li');
+    li.classList.add('adventure-container');
+
+    const coffee = document.createElement('h2');
+    coffee.textContent = adventure.coffeeShop;
+    console.log(adventure);
+}
