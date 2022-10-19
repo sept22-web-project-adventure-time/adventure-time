7d205f5c420006ac9407c734f2f5e4b9a18e055e
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-10-19 09:33:07 -0700

    render added sections and classes on adventure page. little css pazaz too

--- render-utils.js
+++ render-utils.js
@@ -3,19 +3,28 @@ export function renderAdventure(adventure) {
     div.classList.add('adventure-container');
 
     const coffeeSection = document.createElement('section');
+    coffeeSection.classList.add('coffee-section');
+    const coffeeText = document.createElement('p');
+    coffeeText.textContent = 'Grab your coffee at...';
     const coffee = document.createElement('h1');
     coffee.textContent = adventure.coffeeShop;
-    coffeeSection.append(coffee);
+    coffeeSection.append(coffeeText, coffee);
 
     const activitySection = document.createElement('section');
+    activitySection.classList.add('activity-section');
+    const activityText = document.createElement('p');
+    activityText.textContent = 'then...';
     const activity = document.createElement('h1');
     activity.textContent = adventure.activity;
-    activitySection.append(activity);
+    activitySection.append(activityText, activity);
 
     const eaterySection = document.createElement('section');
+    eaterySection.classList.add('eatery-section');
     const eatery = document.createElement('h1');
+    const eateryText = document.createElement('p');
+    eateryText.textContent = 'and finish with a meal at...';
     eatery.textContent = adventure.eatery;
-    eaterySection.append(eatery);
+    eaterySection.append(eateryText, eatery);
 
     div.append(coffeeSection, activitySection, eaterySection);
 
